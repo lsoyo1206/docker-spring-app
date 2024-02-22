2b8d74bbce83e578b537f68a10c35969634072e8
study-dev_011 <lee56244@gmail.com>
2024-02-22 14:27:38 +0900

    jsp 연결

---
+++ src/main/java/admin/controller/AdminController.java
@@ -0,0 +1,15 @@
+package admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AdminController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+<!DOCTYPE html>
+<%--<html lang="ko" xmlns:th="http://www.thymeleaf.org">--%>
+<html>
+<head>
+    <title>스프링 부트 웹 서비스</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+</head>
+<body>
+<h1>스프링 부트로 시작하는 웹 서비스</h1>
+</body>
+</html>
